7a0a68055f4788e0c4f106c4768e9b258f6a3129
IT Brasil <57155283+it-brasil@users.noreply.github.com>
2021-03-18 17:20:49 +0100

    removed image

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,6 @@
     'category': 'Accounting',
     'depends': ['base', 'account', 'stock'],
     'data': ['views/invoice_stock_move_view.xml'],
-    'images': ['static/description/banner.png'],
     'installable': True,
     'auto_install': False,
     'application': True,
